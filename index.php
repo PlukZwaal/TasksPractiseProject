09caa898395987e344bc7737d7de4a405d6f3e0e
Pluk Zwaal <Plukky.zwaal@gmail.com>
2022-11-27 00:45:24 +0100

    added a basic input field with a add button and a clean background for the whole page

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Tasks</title>
+</head>
+<body>
+    <header>
+    </header>
+    <main>
+        <div class="wrapper">
+            <div class="addTask">
+                <input type="text" placeholder="type here the task">
+                <button id="addTaskButton">ADD</button>
+            </div>
+            <div class="tasks">
+            </div>
+        </div>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>

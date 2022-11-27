03e57bfda1fe4b946920b73f80ff8f2b642d1662
Pluk Zwaal <Plukky.zwaal@gmail.com>
2022-11-27 22:45:58 +0100

    the add button now works

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
     <title>Tasks</title>
 </head>
 <body>
@@ -13,10 +15,10 @@
     <main>
         <div class="wrapper">
             <div class="addTask">
-                <input type="text" placeholder="type here the task">
+                <input id="inputTask" type="text" placeholder="type here the task">
                 <button id="addTaskButton">ADD</button>
             </div>
-            <div class="tasks">
+            <div id="tasks" class="tasks">
             </div>
         </div>
     </main>
@@ -24,3 +26,36 @@
     </footer>
 </body>
 </html>
+
+<script>
+    const tasks = [];
+    if(JSON.parse(localStorage.getItem('tasks')) == null){
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'))
+    storedTasks.forEach(function (item, index) {
+        tasks.push(item)
+        document.getElementById('tasks')
+            .innerHTML += `
+            <div class="task">
+                <p>${item}</p>
+                <button class="fa fa-trash-o"></button>
+            </div>
+            `;
+    });
+
+    document.getElementById('addTaskButton')
+        .addEventListener('click', function(e){
+            var inputTaskValue = document.getElementById("inputTask").value;
+            tasks.push(inputTaskValue)
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            document.getElementById('tasks')
+                .innerHTML += `
+                <div class="task">
+                    <p>${inputTaskValue}</p>
+                    <button class="fa fa-trash-o"></button>
+                </div>
+                `;
+        });
+
+</script>
